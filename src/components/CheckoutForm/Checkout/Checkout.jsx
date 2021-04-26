141f27a0b3e19efd99a7e3512ce1cff57a643dbc
codekamat <easykamat@gmail.com>
2021-04-26 13:33:02 +0530

    adding Checkout page

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   Paper,
   Stepper,
@@ -9,22 +9,50 @@ import {
   Divider,
   Button,
 } from '@material-ui/core'
-
+import { commerce } from '../../../lib/commerce'
 import useStyles from './styles'
 import AddressForm from '../AddressForm'
 import PaymentForm from '../PaymentForm'
 
 const steps = ['Shipping address', 'Payment details']
 
-const Checkout = () => {
+const Checkout = ({ cart }) => {
   const [activeStep, setActiveStep] = useState(0)
+  const [checkoutToken, setCheckoutToken] = useState(null)
+  const [shippingData, setShippingData] = useState({})
   const classes = useStyles()
+
+  useEffect(() => {
+    const generateToken = async () => {
+      try {
+        const token = await commerce.checkout.generateToken(cart.id, {
+          type: 'cart',
+        })
+        console.log(token)
+        setCheckoutToken(token)
+      } catch (error) {}
+    }
+    generateToken()
+  }, [cart])
+
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1)
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1)
+
+  const next = (data) => {
+    setShippingData(data)
+    nextStep()
+  }
 
   const Confirmation = () => {
     return <div>Confirmation</div>
   }
 
-  const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />)
+  const Form = () =>
+    activeStep === 0 ? (
+      <AddressForm checkoutToken={checkoutToken} next={next} />
+    ) : (
+      <PaymentForm shippingData={shippingData} />
+    )
 
   return (
     <>
@@ -41,7 +69,11 @@ const Checkout = () => {
               </Step>
             ))}
           </Stepper>
-          {activeStep === steps.length ? <Confirmation /> : <Form />}
+          {activeStep === steps.length ? (
+            <Confirmation />
+          ) : (
+            checkoutToken && <Form />
+          )}
         </Paper>
       </main>
     </>
