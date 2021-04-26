d9d50ac65d1005f982ac851fda4e5667973b3869
codekamat <easykamat@gmail.com>
2021-04-27 00:05:00 +0530

    add order summary in checkout form

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Typography, Button, Divider } from '@material-ui/core'
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from '@stripe/react-stripe-js'
+import { loadStrip } from '@stripe/stripe-js'
 
-const PaymentForm = () => {
-  return <div>PaymentForm</div>
+import Review from './Review'
+
+const PaymentForm = ({ checkoutToken }) => {
+  return (
+    <>
+      <Review checkoutToken={checkoutToken} />
+      <Divider />
+      <Typography variant='h6' gutterBottom style={{ margin: '20px 0' }}>
+        Payment method
+      </Typography>
+    </>
+  )
 }
 
 export default PaymentForm
